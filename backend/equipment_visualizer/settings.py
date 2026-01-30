52a8a5a5fb23429bc4db92cdcbc4f9743f504963
Madhuram2901 <143246077+Madhuram2901@users.noreply.github.com>
2026-01-30 10:36:29 +0530

    Update settings.py

--- backend/equipment_visualizer/settings.py
+++ backend/equipment_visualizer/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-key-change-in-pro
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 INSTALLED_APPS = [
